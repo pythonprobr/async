a2a462112e3dedeb58250f8e2f7851676615a672
Luciano Ramalho <luciano@ramalho.org>
2014-06-04 01:00:01 -0300

    cleanup sync.py example

--- wikipedia/sync.py
+++ wikipedia/sync.py
@@ -15,6 +15,7 @@ import argparse
 import datetime
 import urllib2
 import contextlib
+import time
 
 POTD_BASE_URL = 'http://en.wikipedia.org/wiki/Template:POTD/'
 
@@ -24,6 +25,7 @@ THUMB_SRC_RE = re.compile(r'src=".*?/thumb/(.*?/\d+px-[^"]+)')
 LOCAL_IMG_PATH = 'pictures/'
 
 verbose = True
+
 
 class ParsingException(ValueError):
     """Raised if unable to parse POTD MediaWiki source"""
@@ -36,7 +38,8 @@ def fetch_potd_url(iso_date):
         html = fp.read()
         thumb_src = THUMB_SRC_RE.search(html)
         if not thumb_src:
-            raise ParsingException('cannot find thumbnail source for ' + url)
+            msg = 'cannot find thumbnail source for ' + potd_url
+            raise ParsingException(msg)
         thumb_url = THUMB_BASE_URL+thumb_src.group(1)
     return thumb_url
 
@@ -62,39 +65,51 @@ def get_img_names(iso_month):
             break
         yield (iso_date, img_url)
 
+
+def fetch_image(iso_date, img_url):
+    if verbose:
+        print('\t' + img_url)
+    with contextlib.closing(urllib2.urlopen(img_url)) as fp:
+        img = fp.read()
+    img_filename = iso_date + '__' + img_url.split('/')[-1]
+    if verbose:
+        print('\t\twriting %0.1f Kbytes' % (len(img)/1024.0))
+    img_path = os.path.join(LOCAL_IMG_PATH, img_filename)
+    with io.open(img_path, 'wb') as fp:
+        fp.write(img)
+    return len(img)
+
+
 def get_images(iso_month, max_count=0):
     if max_count is 0:
         max_count = sys.maxsize
     img_count = 0
     total_size = 0
     for iso_date, img_url in get_img_names(iso_month):
-        if verbose:
-            print('\t' + img_url)
-        with contextlib.closing(urllib2.urlopen(img_url)) as fp:
-            img = fp.read()
+        total_size += fetch_image(iso_date, img_url)
         img_count += 1
-        total_size += len(img)
-        img_filename = iso_date + '__' + img_url.split('/')[-1]
-        if verbose:
-            print('\t\twriting %0.1f Kbytes' % (len(img)/1024.0))
-        img_path = os.path.join(LOCAL_IMG_PATH, img_filename)
-        with io.open(img_path, 'wb') as fp:
-            fp.write(img)
         if img_count == max_count:
             break
+
     return (img_count, total_size)
 
+
 def main():
+    """Get "Pictures of The Day" from English Wikipedia for a given month"""
     global verbose
-    parser = argparse.ArgumentParser(description='Get "Pictures of The Day" from English Wikipedia for a given month')
+    parser = argparse.ArgumentParser(description=main.__doc__)
     parser.add_argument('year_month', help='year and month in YYYY-MM format')
-    parser.add_argument('-q', '--max_qty', help='maximum number of files to download', type=int)
-    parser.add_argument('-v', '--verbose', help='display progress information', action='store_true')
+    parser.add_argument('-q', '--max_qty', type=int,
+                        help='maximum number of files to download')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='display progress information')
     args = parser.parse_args()
     verbose = args.verbose
+    t0 = time.time()
     img_count, total_size = get_images(args.year_month, args.max_qty)
-    print('%s images downloaded (%0.1f Kbytes total)' %
-            (img_count, total_size/1024.0))
+    elapsed = time.time() - t0
+    print("images: %3d |  total size: %6.1f Kbytes  |  elapsed time: %3ds" %
+          (img_count, total_size/1024.0, elapsed))
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
