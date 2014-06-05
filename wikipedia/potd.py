6f87e840db02bb7804917c016269e75adc28b421
Luciano Ramalho <luciano@ramalho.org>
2014-06-05 17:20:17 -0300

    potd.py versao sync ok

--- wikipedia/potd.py
+++ wikipedia/potd.py
@@ -1,13 +1,21 @@
 import calendar
 import datetime
 import re
+import os
+import io
+import time
+
 import requests
 
-def build_page_url(date):
-    base_url = 'http://en.wikipedia.org/wiki/Template:POTD/'
-    return base_url + date
+import argparse
 
-def fetch_page(url):
+SAVE_DIR = 'pictures/'
+
+def build_page_url(iso_date):
+    base_url = 'http://en.wikipedia.org/wiki/Template:POTD/'
+    return base_url + iso_date
+
+def fetch(url):
     response = requests.get(url)
     return response
 
@@ -16,14 +24,60 @@ def extract_image_url(html):
     image_url = re.search(re_image, html)
     return 'http:' + image_url.group(1)
 
-def get_days_of_month(year, month):
-    lastday = calendar.monthrange(year, month)[1]
-    days = [x for x in range(1, lastday + 1)]
-    return days
-
 def format_date(year, month, day):
     return '{year}-{month:02d}-{day:02d}'.format(**locals())
 
-# def get_iso_dates(year, month):
-#     days = get_days_of_month(year, month)
+def get_days_of_month(year, month):
+    lastday = calendar.monthrange(year, month)[1]
+    days = [format_date(year, month, day) for day in range(1, lastday + 1)]
+    return days
 
+def build_save_path(iso_date, url):
+    head, filename = os.path.split(url)
+    return os.path.join(SAVE_DIR, iso_date+'_'+filename)
+
+def save_one(iso_date):
+    page_url = build_page_url(iso_date)
+    response = fetch(page_url)
+    img_url = extract_image_url(response.text)
+    response = fetch(img_url)
+    path = build_save_path(iso_date, img_url)
+    if verbose:
+        print('saving: '+path)
+    with io.open(path, 'wb') as fp:
+        fp.write(response.content)
+    return len(response.content)
+
+def save_month(year_month):
+    year, month = [int(s) for s in year_month.split('-')]
+    total_size = 0
+    dates = get_days_of_month(year, month)
+    for date in dates:
+        total_size += save_one(date)
+    return len(dates), total_size
+
+def main():
+    """Get "Picture of The Day" from English Wikipedia for a given date or month"""
+    global verbose
+    parser = argparse.ArgumentParser(description=main.__doc__)
+    parser.add_argument('date', help='year, month and (optional) day in YYYY-MM-DD format')
+    parser.add_argument('-q', '--max_qty', type=int,
+                        help='maximum number of files to download')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='display progress information')
+    args = parser.parse_args()
+    verbose = args.verbose
+
+    t0 = time.time()
+    if len(args.date) == len('YYYY-MM-DD'):
+        img_count = 1
+        total_size = save_one(args.date)
+    else:
+        img_count, total_size = save_month(args.date)
+    elapsed = time.time() - t0
+    print("images: %3d |  total size: %6.1f Kbytes  |  elapsed time: %3ds" %
+          (img_count, total_size/1024.0, elapsed))
+
+
+if __name__ == '__main__':
+    main()
--- wikipedia/potd_tests.py
+++ wikipedia/potd_tests.py
@@ -6,56 +6,78 @@ import potd
 class TestSequenceFunctions(unittest.TestCase):
 
     def setUp(self):
-        self.html = '''<td><a href="/wiki/File:Obama_and_Biden_await_updates_on_bin_Laden.jpg" class="image" title="Situation Room"><img alt="Situation Room" src="//upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Obama_and_Biden_await_updates_on_bin_Laden.jpg/400px-Obama_and_Biden_await_updates_on_bin_Laden.jpg" width="400" height="267" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Obama_and_Biden_await_updates_on_bin_Laden.jpg/600px-Obama_and_Biden_await_updates_on_bin_Laden.jpg 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Obama_and_Biden_await_updates_on_bin_Laden.jpg/800px-Obama_and_Biden_await_updates_on_bin_Laden.jpg 2x" data-file-width="4096" data-file-height="2731" /></a></td><td></td>'''
-        pass
+        self.thumb_url = ("""http://upload.wikimedia.org/wikipedia/"""
+            """commons/thumb/f/fe/Orthographic_projection_SW.jpg/350px"""
+            """-Orthographic_projection_SW.jpg""")
 
     def test_buid_page_url(self):
         date = '2014-05-01'
         result = potd.build_page_url(date)
         self.assertEqual(result, 'http://en.wikipedia.org/wiki/Template:POTD/2014-05-01')
 
-    def test_potd_fetch_page_status_code(self):
+    def test_fetch_status_code(self):
         date = '2014-05-02'
         url = potd.build_page_url(date)
-        response = potd.fetch_page(url)
+        response = potd.fetch(url)
         self.assertEqual(response.status_code, 200)
 
-    def test_potd_extract_image_url(self):
-        image_url = potd.extract_image_url(self.html)
-        self.assertEqual(image_url, 'http://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Obama_and_Biden_await_updates_on_bin_Laden.jpg/400px-Obama_and_Biden_await_updates_on_bin_Laden.jpg')
+    def test_fetch_status_code_not_found(self):
+        date = '2100-01-01'
+        url = potd.build_page_url(date)
+        response = potd.fetch(url)
+        self.assertEqual(response.status_code, 404)
 
-    def test_potd_fetch_image_jpeg(self):
-        image_url = 'http://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Obama_and_Biden_await_updates_on_bin_Laden.jpg/400px-Obama_and_Biden_await_updates_on_bin_Laden.jpg'
-        response = potd.fetch_page(image_url)
+    def test_extract_image_url(self):
+        image_url = potd.extract_image_url(HTML)
+        self.assertEqual(image_url, self.thumb_url)
+
+    def test_fetch_image_jpeg(self):
+        response = potd.fetch(self.thumb_url)
         self.assertEqual(response.headers['content-type'], 'image/jpeg')
 
-    def test_potd_get_days_of_month(self):
+    def test_get_days_of_month(self):
         year = 2014
         month = 5
         days = potd.get_days_of_month(year, month)
         self.assertEqual(len(days), 31)
-        self.assertEqual(1, days[0])
-        self.assertEqual(31, days[-1])
+        self.assertEqual('2014-05-01', days[0])
+        self.assertEqual('2014-05-31', days[-1])
 
-    def test_potd_get_days_of_february(self):
+    def test_get_days_of_february(self):
         year = 2014
         month = 2
         days = potd.get_days_of_month(year, month)
         self.assertEqual(len(days), 28)
-        self.assertEqual(1, days[0])
-        self.assertEqual(28, days[-1])
+        self.assertEqual('2014-02-01', days[0])
+        self.assertEqual('2014-02-28', days[-1])
 
-    def test_potd_format_date(self):
+    def test_format_date(self):
         year = 2014
         month = 2
         day = 1
-        first_date = '2014-02-01'
+        a_date = '2014-02-01'
         date = potd.format_date(year, month, day)
-        self.assertEqual(first_date, date)
+        self.assertEqual(a_date, date)
         self.assertEqual(potd.format_date(2010, 11, 12), '2010-11-12')
 
+    def test_build_save_path(self):
+        date = '2014-06-04'
+        path = potd.SAVE_DIR + date + '_350px-Orthographic_projection_SW.jpg'
+        self.assertEqual(path, potd.build_save_path(date, self.thumb_url))
 
 
+HTML = (
+'''<td><a href="/wiki/File:Orthographic_projection_SW.jpg" class="image"
+title="Orthographic projection"><img alt="Orthographic projection"
+src="//upload.wikimedia.org/wikipedia/commons/thumb/f/fe/O'''
+'''rthographic_projection_SW.jpg/350px-Orthographic_projection_SW.jpg"
+width="350" height="350" srcset="//upload.wikimedia.org/wikipedia/comm'''
+'''ons/thumb/f/fe/Orthographic_projection_SW.jpg/525px-
+Orthographic_projection_SW.jpg 1.5x, //upload.wikimedia.org/wikipedia/
+commons/thumb/f/fe/Orthographic_projection_SW.jpg/700px-
+Orthographic_projection_SW.jpg 2x" data-file-width="2058" data-file-
+height="2058"></a></td>
+''')
 
 if __name__ == '__main__':
     unittest.main()
